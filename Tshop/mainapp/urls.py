9c6aa97de1159e54eae2b51d70a4a64dd225f764
PATIL SURENDRA REDDY <surendrasr5416@gmail.com>
2026-03-26 18:02:08 +0530

    updated

--- Tshop/mainapp/urls.py
+++ Tshop/mainapp/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import homeView,aboutView,contactView
 
 
+from .views import(CarouselImageList)
+
 urlpatterns = [
     path('',homeView,name = 'home_page'),
     path('about/',aboutView,name = 'about_page'),
     path('contact/',contactView,name = 'contact_page'),
+
+    path('carousels/', CarouselImageList.as_view(), name = 'carousel_list')
 ]
--- Tshop/mainapp/views.py
+++ Tshop/mainapp/views.py
@@ -1,35 +1,52 @@
 from django.shortcuts import render
 from .models import CarouselImage
+
+# Views.py handles request-response
+# It also handles the DML and DQL operations required for the same.
+
 # Create your views here.
 
 def homeView(request):
     template = 'mainapp/home.html'
     context = {
+        # This will be an array of all active carousel image objects mapped from DB
         'carousel_images' : CarouselImage.objects.filter(is_active = True),
-        'name' : "Tea Shop"
     }
+
     return render(
         request = request,
-        template_name = template,
-        context=context
+        template_name= template,
+        context= context
     )
 
 def aboutView(request):
     template = 'mainapp/about.html'
 
-
     return render(
         request = request,
-        template_name = template,
+        template_name= template,
         context={}
     )
 
 def contactView(request):
     template = 'mainapp/contact.html'
 
-
     return render(
         request = request,
-        template_name = template,
+        template_name= template,
         context={}
     )
+
+
+# Class based generic views
+from django.views.generic import (
+    CreateView,
+    ListView, DetailView,
+    UpdateView, 
+    DeleteView
+)
+
+class CarouselImageList(ListView):
+    template_name = 'mainapp/carousel/carousel_list.html'
+    model = CarouselImage
+    context_object_name = 'carousel_images'    
